02e080b616fcbbaf8a825f38bc420257c4c06e07
Jacob A. Hudson <jacob.alan.hudson@gmail.com>
2016-12-18 15:37:08 -0600

    Update tmp.sh

--- tmp.sh
+++ tmp.sh
@@ -5,5 +5,7 @@ sudo apt-get install git
 
 #creates a local git dir
 mkdir git
+
+#TODO:  Make this conditional for WSL Only!!  WSL Defaults to Ubuntu 14.04 LTS (at least of 2016.12.18)
 #create a symlink to git for WSL
 ln -s /mnt/c/Users/Jacob/git/* git/
